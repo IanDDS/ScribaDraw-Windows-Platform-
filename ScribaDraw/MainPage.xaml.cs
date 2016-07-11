46fe70c0b1cae7d5a336e96c5ae5501b4efdf1e4
IanDDS <pearse.iancunningham@gmail.com>
2016-07-11 15:38:18 +0100

    Add project files.

---
+++ ScribaDraw/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ScribaDraw
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public Library library = new Library();
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            library.Init(ref Display, ref Size, ref Colour);
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e) { 
+            library.New(Display);
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            library.Open(Display);
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            library.Save(Display);
+        }
+
+        private void Bluetooth_Click(object sender, RoutedEventArgs e)
+        {
+            library.Bluetooth();
+        }
+
+        private void Colour_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            library.Colour(ref Display, ref Colour);
+        }
+
+        private void Size_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            library.Size(ref Display, ref Size);
+        }
+
+    }
+}
